ba2b2741a522d72b89353e03387657de87a5465b
James <xu.Fan@yourdream.cc>
2014-07-01 22:46:35 +0800

    初始化银行培训项目

---
+++ bank/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '69.85.84.105';
+$cfg_dbname = 'shouzhu_xs';
+$cfg_dbuser = 'shouzhu';
+$cfg_dbpwd = 'ZTFlVydN27';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
