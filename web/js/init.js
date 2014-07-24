991c08ccc6a1749e18adb9d635f296816b0173b6
James <xu.fan@yourdream.cc>
2014-07-24 17:27:11 +0800

    修改js

--- web/js/init.js
+++ web/js/init.js
@@ -2,7 +2,19 @@ $.get( '/web/api.php?methodPath=index' , {} , function( data ){
     var strHtml = '';
     for( var i = 0 ; i < data.menu.length ; i++ )
     {
-        strHtml +=  '<li><a href="#!/page_HOME"><b></b><span>' + data.menu[i].name + '</span></a></li>';
+        strHtml +=  '<li><a href="#"><b></b><span>' + data.menu[i].name + '</span></a>';
+        if( data.menu[i].child instanceof Array
+            ||
+            data.menu[i].child instanceof Object
+            )
+        {
+              strHtml += '<div class="submenu"><ul>';
+              for( var j = 0 ; j < data.menu[i].child.length ; j++ )
+              {
+                   strHtml += '<li><a href="#"><span>' + data.menu[i].child[j].name + '</span><b></b></a></li>';
+              }
+              strHtml += '</ul></div></li>';
+        }
     }
     $('#menu').html( strHtml );
 } , 'json' );
--- web/server/index.php
+++ web/server/index.php
@@ -13,7 +13,7 @@ function run()
 
 function _iniMenu()
 {
-	return array(
+	$menu =  array(
 		array( 'id' => 0 , 'pid' => 0 , 'name' => '关于我' ),
 		array( 'id' => 1 , 'pid' => 0 , 'name' => '联系我们' ),
 		array( 'id' => 2 , 'pid' => 0 , 'name' => '最新动态' ),
@@ -23,5 +23,38 @@ function _iniMenu()
 		array( 'id' => 6 , 'pid' => 3 , 'name' => '图片' ),
 		array( 'id' => 7 , 'pid' => 3 , 'name' => '视频' ),
 	);
+	foreach( array_keys( $menu ) as $key )
+	{
+		if( $menu[$key]['pid'] == 0 )
+		{
+			continue;
+		}
+		appendChildTree( $menu , $menu[$key] );
+		unset( $menu[$key] );
+	}
+	return $menu;
+}
+function appendChildTree( &$data , $tree )
+{
+	if( empty( $data ) )
+	{
+		return false;
+	}
+	foreach( $data as $key => $val )
+	{
+		if( $val['id'] == $tree['pid'] )
+		{
+			$data[$key]['child'][] = $tree;
+			return true;
+		}
+		if( isset( $val['child'] ) && is_array( $val['child'] ) )
+		{
+			if( appendChildTree( $data[$key]['child'] , $tree ) )
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
 ?>
