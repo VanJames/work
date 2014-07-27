01d8c9520768c38fe8d6451571c5c2f569524ce7
James <xu.fan@yourdream.cc>
2014-07-27 18:59:43 +0800

    部分修改

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xu
+ * Date: 7/13/14
+ * Time: 12:45 AM
+ */
+Program::Main();
+
+class Program
+{
+	/**
+	 * 程序主入口
+	 * @param array $args
+	 */
+	public static function Main( $args = array() )
+    {
+	    for( $n = 100 ; $n >= 0 ; $n-- )
+	    {
+		    
+	    }
+    }
+}
+?>
--- web/js/init.js
+++ web/js/init.js
@@ -1,20 +1,47 @@
-$.get( '/web/api.php?methodPath=index' , {} , function( data ){
-    var strHtml = '';
-    for( var i = 0 ; i < data.menu.length ; i++ )
+//$.get( '/web/api.php?methodPath=index' , {} , function( data ){
+//    var strHtml = '';
+//    for( var i = 0 ; i < data.menu.length ; i++ )
+//    {
+//        strHtml +=  '<li><a href="#"><b></b><span>' + data.menu[i].name + '</span></a>';
+//        if( data.menu[i].child instanceof Array
+//            ||
+//            data.menu[i].child instanceof Object
+//            )
+//        {
+//              strHtml += '<div class="submenu"><ul>';
+//              for( var j = 0 ; j < data.menu[i].child.length ; j++ )
+//              {
+//                   strHtml += '<li><a href="#"><span>' + data.menu[i].child[j].name + '</span><b></b></a></li>';
+//              }
+//              strHtml += '</ul></div></li>';
+//        }
+//    }
+//    $('#menu').html( strHtml );
+//} , 'json' );
+var initArray = [];
+var initTime = 1;
+$('#menu').find('li').find('a').live('click',function(){
+    var id = $(this).attr('data-id');
+    checkAndBind(id);
+    location.href=$(this).attr('data-href');
+});
+function bind(id){
+    $('#'+id).find('.box').append('<p>123</p>');
+}
+
+function checkAndBind(id)
+{
+    initTime++;
+    console.log(initTime);
+    if(initTime >= 24 )
     {
-        strHtml +=  '<li><a href="#"><b></b><span>' + data.menu[i].name + '</span></a>';
-        if( data.menu[i].child instanceof Array
-            ||
-            data.menu[i].child instanceof Object
-            )
-        {
-              strHtml += '<div class="submenu"><ul>';
-              for( var j = 0 ; j < data.menu[i].child.length ; j++ )
-              {
-                   strHtml += '<li><a href="#"><span>' + data.menu[i].child[j].name + '</span><b></b></a></li>';
-              }
-              strHtml += '</ul></div></li>';
-        }
+        location.reload();
+        return ;
     }
-    $('#menu').html( strHtml );
-} , 'json' );
+    if($.inArray(id,initArray) != -1 )
+    {
+        return;
+    }
+    initArray.push(id);
+    bind(id);
+}
